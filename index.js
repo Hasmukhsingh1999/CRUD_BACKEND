54c7da86cdf26bb5406085ddf870fd4f165601c2
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 21:18:15 +0530

    ...

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 
-app.listen(8800,()=>{
-    console.log(`Server started at ${8800}`)
+dotenv.config();
+
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`)
 })
