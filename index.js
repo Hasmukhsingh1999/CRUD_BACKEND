58eb1845f06add81f7ffb70bf9da52d3477815ba
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 22:45:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import mysql from 'mysql'
+import mysql from "mysql2";
 const app = express();
 
 dotenv.config();
@@ -12,9 +12,17 @@ const db = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
-app.get("/",(req,res)=>{
-    res.json("hello this")
-})
+app.get("/", (req, res) => {
+  res.json("hello this");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
