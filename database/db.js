3f8d1cd2f0533cb9bac65ba1a30ce9125a339f6e
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 22:32:37 +0530

    ...

--- database/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql";
-
-export const connectDB = async () => {
-  const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-  });
-};
--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express();
 
 dotenv.config();
 
-
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT,()=>{
