193f612560bd23c93b89c57d30ea855ff6c1a284
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 22:33:58 +0530

    ...

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
+import express from "express";
+import dotenv from "dotenv";
+import mysql from 'mysql'
 const app = express();
 
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  });
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT,()=>{
-    console.log(`Server started at ${PORT}`)
+app.get("/",(req,res)=>{
+    res.json("hello this")
 })
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
