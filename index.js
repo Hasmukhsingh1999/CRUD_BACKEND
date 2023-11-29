0535273eb6ba9a104c4d0fc887a4268cab4df999
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 10:20:02 +0530

    added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2";
-import cors from 'cors';
+import cors from "cors";
+import morgan from "morgan";
 const app = express();
-
 
 dotenv.config();
 
@@ -14,8 +14,10 @@ const db = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
-app.use(express.json());
+app.use(express());
 app.use(cors());
+app.use(morgan());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("hello this");
@@ -32,21 +34,25 @@ app.get("/books", (req, res) => {
 // create a book ->
 app.post("/books", (req, res) => {
   const { title, desc, cover } = req.body;
-
   if (!title || !desc || !cover) {
     return res.status(400).json({ error: "Missing required fields" });
   }
-
   const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?, ?, ?)";
   const values = [title, desc, cover];
-
   db.query(q, values, (err, data) => {
     if (err) {
       console.error("Error creating book:", err);
       return res.status(500).json({ error: "Internal Server Error" });
     }
-
     return res.status(201).json({ message: "Book created successfully", data });
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookID = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+  db.query(q, [bookID], (err, data) => {
+    res.json({ message: "Books have been deleted!" });
   });
 });
 
