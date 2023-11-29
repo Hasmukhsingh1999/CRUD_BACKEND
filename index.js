c3963b63fd57d23a1d9781bc9f79aa7ec3180b30
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 09:39:29 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import cors from 'cors';
 const app = express();
+
 
 dotenv.config();
 
@@ -11,6 +13,9 @@ const db = mysql.createConnection({
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello this");
@@ -26,24 +31,23 @@ app.get("/books", (req, res) => {
 
 // create a book ->
 app.post("/books", (req, res) => {
-    const { title, desc, cover } = req.body; // Assuming your request body contains these fields
+  const { title, desc, cover } = req.body;
 
-    // Ensure all required fields are present
-    if (!title || !desc || !cover) {
-      return res.status(400).json({ error: "Missing required fields" });
+  if (!title || !desc || !cover) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?, ?, ?)";
+  const values = [title, desc, cover];
+
+  db.query(q, values, (err, data) => {
+    if (err) {
+      console.error("Error creating book:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-  
-    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?, ?, ?)";
-    const values = [title, desc, cover];
-  
-    db.query(q, values, (err, data) => {
-      if (err) {
-        console.error("Error creating book:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-  
-      return res.status(201).json({ message: "Book created successfully", data });
-    });
+
+    return res.status(201).json({ message: "Book created successfully", data });
+  });
 });
 
 const PORT = process.env.PORT || 4000;
